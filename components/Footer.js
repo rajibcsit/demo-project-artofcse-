8a4a93af17daf07549f8b9611ab563a929de611c
rajibcsit <18192203021@cse.bubt.edu.bd>
2023-01-21 18:04:28 +0600

    create a about page and route

--- components/Footer.js
+++ components/Footer.js
@@ -5,14 +5,14 @@ function Footer() {
       <section id="navbar">
         <nav class="p-5 bg-black shadow md:flex md: items-center md: justify-between">
           <p class="text-green-500">
-            Copyrights © 2022 All Reserved by
+            Copyrights © 2023 All Reserved by
             <span class="text-white hover:text-cyan-500 "> Art of CSE </span>
           </p>
 
           <div class="text-right">
             <ul class="md:flex md:items-center md:z-auto md:static absolute bg-black w-full left-0 md:py-0 py-4 md:pl-0 p1-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
-              <Link href="">
-                <li class="mx-4 my-6 md:my-0 text-base text-lime-500 hover:text-cyan-500 duration-500">
+              <Link href="/info/about">
+                <li class="mx-4 my-6 md:my-0 text-base text-white hover:text-cyan-500 duration-500">
                   About
                 </li>
               </Link>
---
+++ pages/info/about.js
@@ -0,0 +1,42 @@
+function About() {
+  return (
+    <div>
+      <>
+        <section id="intro" class="bg-gradient-to-r from-cyan-500 to-green-200">
+          <div class="container">
+            <div class="grid place-items-center pt-10 pb-6 ">
+              <div class="grid place-items-center">
+                <h1 class="text-2xl text-white font-semibold"> Welcome To Art of CSE</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="about bg-white pb-80">
+          <div class="container grid place-items-center justify-center">
+            <div class="grid place-items-center justify-center pt-7">
+              <p class="text-black text-sm">
+                artofcse.com is a programming based community which focuses on several aspects of
+                computer science and engineering such as programming, data structure and algorithm,
+                problem-solving etc. Beside all these, it provides web design, web development and
+                application development courses to build up creative and productive programmers and
+                developers. <br /> <br />
+                We believe CSE is a creative subject to show off ones talent and to build up new
+                technologies in order to make human life easier and comfortable.
+                <br /> <br /> From that feeling, artofcse.com emerged in 2016 with a community
+                including some skilled trainers and programmers which have been committed to
+                heightening programming skills together through exchanging knowledge. <br /> <br />
+                Artofcse always welcomes your idea, opinion and contribution to make this community
+                better. Contact us <br /> <br />
+                Find our course to take your programming and development skills to the next level.
+                Our Courses
+              </p>
+            </div>
+          </div>
+        </section>
+      </>
+    </div>
+  );
+}
+
+export default About;
